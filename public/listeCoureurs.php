b3ee1163382130950bfe3b1d855a2a8922416012
PHILIPPE LAMORSKI <philippe.lamorskipro@gmail.com>
2020-03-08 11:53:22 +0100

    rectif delete

--- public/listeCoureurs.php
+++ public/listeCoureurs.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once'../include/connectBdd.php';
-require_once'../include/header.php';
+require_once '../include/connectBdd.php';
+require_once '../include/header.php';
 
 $liste = $bdd->query('SELECT * FROM coureurs');
 $liste->execute();
@@ -12,7 +12,8 @@ $liste->execute();
     <h3 class="card-header text-center font-weight-bold text-uppercase py-4"></h3>
     <div class="card-body">
         <div id="table-contact" class="table-editable">
-            <span class="table-add float-right mb-3 mr-2"><a href="#!" class="text-success"><i class="fas fa-plus fa-2x" aria-hidden="true"></i></a></span>
+            <span class="table-add float-right mb-3 mr-2"><a href="#!" class="text-success"><i class="fas fa-plus fa-2x"
+                        aria-hidden="true"></i></a></span>
             <table id="table-contact" class="table table-bordered table-responsive-md table-striped text-center">
                 <thead>
                     <tr>
@@ -24,16 +25,12 @@ $liste->execute();
                         <th class="text-center">ville</th>
                         <th class="text-center">code postal</th>
                         <th class="text-center">date de naissance</th>
-
-
-
-
                     </tr>
                 </thead>
                 <tbody>
                     <?php
                     while ($listes = $liste->fetch()) {
-                        ?>
+                    ?>
                     <tr>
                         <td class="pt-3-half" contenteditable="true"><?= $listes['name'] ?></td>
                         <td class="pt-3-half" contenteditable="true"><?= $listes['surname'] ?></td>
@@ -44,28 +41,27 @@ $liste->execute();
                         <td class="pt-3-half" contenteditable="true"><?= $listes['postalCode'] ?></td>
                         <td class="pt-3-half" contenteditable="true"><?= $listes['birthdayDate'] ?></td>
                         <td>
-                    <a class="" href="../treatment/deleteCoureur.php?id=<?= $listes['id'] ?>">
-                        <button type="button" class="btn btn-danger deleteQuestions">Supprimer</button>
-                    </a>
-                </td>
-                <td>
-                    <a class="" target="#exampleModal" href="../treatment/updateCoureur.php?id=<?= $listes['id'] ?>">
-                        <button type="button" data-toggle="modal" data-target="#myModal"class="btn btn-secondary deleteQuestions">Modifier</button>
-                    </a>
-                </td>
+                            <a class="btn btn-danger" type="button"
+                                href="../treatment/deleteCoureur.php?id=<?= $listes['id'] ?>">
+                                Supprimer
+                            </a>
+                        </td>
+                        <td>
+                            <a class="btn btn-success" type="button"
+                                href="../treatment/updateCoureur.php?id=<?= $listes['id'] ?>">
+                                Modifier
+                            </a>
+                        </td>
 
-                        
 
-                       
-                        
+
+
+
                         <?php
-                        }
-                       
-?>
-                        
+                    }
+
+                        ?>
+
                 </tbody>
             </table>
         </div>
-
-
-        
