5d6204c0cb700c94dc935ea8bb178f5e4e12bb3b
PHILIPPE LAMORSKI <philippe.lamorskipro@gmail.com>
2020-03-08 12:07:44 +0100

    recup id update coureurs

--- treatment/updateCoureur.php
+++ treatment/updateCoureur.php
@@ -1,69 +1,63 @@
 <?php
 require_once'../include/connectBdd.php';
-require_once'../include/header.php';
-$upCoureur = $bdd->query('SELECT * FROM coureurs');
-$upCoureur->execute();
-?>
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content bg-secondary text-warning">
-            <div class="modal-header">
-                <h5 class="modal-title text-uppercase" id="exampleModalLabel"></h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <!-- Modal-formulaire Modif_Question -->
-                <form id="formModif" action="update_questions.php" method="post">
-                    <label class="font-weight-bold" for="intitule">Intitulé :</label>
-                    <input name="intitule" type="text" placeholder="Intitulé" class="form-control" id="intitule"
-                            required aria-label="Sizing example input"
-                            aria-describedby="inputGroup-sizing-default"><br>
-                    <label class="font-weight-bold" for="type">Type de champs:</label>
-                    <select class="form-control" name="type" id="type">
-                        <?php
-                        while ($donnees = $upCoureur->fetch()) {
-                            ?>
-                            <option value='<?= $donnees['id'] ?>'> ?></option>
-                            <?php
-                        }
-                        ?>
-                    </select><br>
-                    <label class="font-weight-bold" for="valeur">Question :</label>
-                    <input type="hidden" id="id" name="id"/>
-                    <input type="hidden" name="action" value="updateCoureur"/>
-                    <input name="valeur" id="valeur" type="text" placeholder="" class="form-control"
-                            required aria-label="Sizing example input"
-                            aria-describedby="inputGroup-sizing-default"><br>
-                </form>
+ require_once'../include/header.php';
 
+$id = intval($_GET['id']);
+$reponse = $bdd->prepare('SELECT * From coureurs WHERE id = ' . $id);
+$reponse->execute();
+$donnees = $reponse->fetch()
+
+?>
+
+<div class="col-6 d-flex justify-content-center">
+<form id="add_user_form" action="../treatment/traitement.php" method="POST" class="mx-2">
+        <div class="d-md-flex">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="name">Nom</label>
+                    <input type="text" value="<?= $donnees['name'] ?>"class="form-control" id="name" name="name" placeholder="Nom">
+                </div>
+                <div class="form-group">
+                    <label for="surname">Prénom</label>
+                    <input type="text" value="<?= $donnees['surname'] ?>"" class="form-control" id="surname" name="surname" placeholder="Prénom">
+                </div>
+                <div class="form-group">
+                    <label for="mail">Email</label>
+                    <input type="email" value="<?= $donnees['email'] ?>"" class="form-control" id="mail" name="email" aria-describedby="emailHelp" placeholder="Email">
+                </div>   
             </div>
-            <div class="modal-footer">
-                <button type="submit" id="subpost" value="Envoyer" class="btn btn-warning">Valider les
-                    modifications
-                </button>
-                <button type="button" class="btn btn-primary" data-dismiss="modal">Fermer</button>
+            <div class="col-md-6">
+                <div class="d-flex">
+                    <div class="form-group col-2 pl-0 pr-1">
+                        <label for="street_number">N°</label>
+                        <input type="text" value="<?= $donnees['streetNumber'] ?>" class="form-control" id="streetNumber" name="streetNumber" placeholder="N°">
+                    </div>
+                    <div class="form-group col-10 px-0">
+                        <label for="street_name">Nom de Rue</label>
+                        <input type="text" value="<?= $donnees['street'] ?>" class="form-control" id="streetName" name="streetName" placeholder="Nom de Rue">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="postal_code">Code Postal</label>
+                    <input type="text" value="<?= $donnees['postalCode'] ?>" class="form-control" id="postalCode" name="postalCode" placeholder="Code Postal">
+                </div>
+                <div class="form-group">
+                    <label for="city">Ville</label>
+                    <input type="text" value="<?= $donnees['city'] ?>"class="form-control" id="city" name="city" placeholder="Ville">
+                </div>
+                <div class="form-group">
+                    <label for="city">Date de naissance</label>
+                    <input type="date" value="<?= $donnees['birthdayDate'] ?>" class="form-control" id="city" name="birthdayDate" placeholder="Date de naissance">
+                </div>
+                
             </div>
         </div>
-    </div>
+        <div class=" mx-2 d-flex justify-content-end">
+            <button id="btnInscription" name="registration" type="submit" class="btn text-dark font-weight-bold mb-5">Modifier</button>
+        </div>
+    </form>
+
 </div>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php require_once'../include/footer.php';
+<?php require_once'../include/footer.php';?>
